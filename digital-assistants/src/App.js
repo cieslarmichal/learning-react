be68a10d3bcdb9aa237cd475ae493f15fe9fdd59
Michal Cieslar <michalovskyyy@gmail.com>
2024-10-18 13:40:50 +0200

    feat: add css styles

--- digital-assistants/src/App.js
+++ digital-assistants/src/App.js
@@ -1,3 +1,5 @@
+import 'bulma/css/bulma.css';
+
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
@@ -6,14 +8,44 @@ import SiriImage from "./images/siri.png";
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <h1 className="title">Personal Digital Assistants</h1>
+        </div>
+      </section>
 
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}/>
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage}/>
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImage}/>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard 
+                title="Alexa" 
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa is a virtual assistant that controls smart devices, but it can also play music, answer questions, read the news, and more."
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Cortana is a virtual assistant that can help you with tasks, answer questions, set reminders, and more."
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri is a virtual assistant that can send messages, make calls, set reminders, and more."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
-
 
 export default App;
