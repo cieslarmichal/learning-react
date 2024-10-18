ee11b6710a0185ba7961e51ac6bc0cf795ff63af
Michal Cieslar <michalovskyyy@gmail.com>
2024-10-18 14:53:24 +0200

    feat: add animals images

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,7 +1,24 @@
-function AnimalShow() {
+import BirdImage from './images/bird.svg';
+import CatImage from './images/cat.svg';
+import CowImage from './images/cow.svg';
+import DogImage from './images/dog.svg';
+import GatorImage from './images/gator.svg';
+import HorseImage from './images/horse.svg';
+import HeartImage from './images/heart.svg';
+
+const imagesMapping = {
+  bird: BirdImage,
+  cat: CatImage,
+  cow: CowImage,
+  dog: DogImage,
+  gator: GatorImage,
+  horse: HorseImage,
+};
+
+function AnimalShow({type, index}) {
   return (
     <div>
-      Cow
+      <img src={imagesMapping[type]} alt={type} />
     </div>
   );
 }
--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,27 @@
+import {useState} from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    console.log('Add Animal');
+    setAnimals([...animals, getRandomAnimal()]);
   };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index}/>;
+  });
 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
