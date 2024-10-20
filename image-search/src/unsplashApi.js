e73d8cafac9c6ab48316c71eac4ffab817cde11d
Michal Cieslar <michalovskyyy@gmail.com>
2024-10-20 17:05:56 +0200

    fix: fix typo

--- image-search/src/unsplashApi.js
+++ image-search/src/unsplashApi.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const searchImages = async (query) => {
-  console.log({token: process.env.REACT_APP_UNSPLASH_ACCESS_KEY})
   const response = await axios.get('https://api.unsplash.com/search/photos', {
       headers:{
         Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
