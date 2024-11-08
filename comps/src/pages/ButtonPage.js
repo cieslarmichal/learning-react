a7dc88ba7da8c2a98e057378ab6def7d89f6c145
Michal Cieslar <michalovskyyy@gmail.com>
2024-11-08 13:10:42 +0100

    refactor: refactor components

--- comps/src/pages/ButtonPage.js
+++ comps/src/pages/ButtonPage.js
@@ -1,7 +1,7 @@
 import {GoBell, GoCloudDownload, GoDatabase, GoHeart} from "react-icons/go";
-import Button from "./components/Button";
+import Button from "../components/Button";
 
-function App () {
+function ButtonPage () {
   const handleClick = () => {
     console.log("Button clicked");
   }
@@ -24,4 +24,4 @@ function App () {
   );
 }
 
-export default App;
+export default ButtonPage;
