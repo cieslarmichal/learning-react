c126729182dd25a6787804ed6beb060377e53f3c
Michal Cieslar <michalovskyyy@gmail.com>
2024-11-17 20:27:01 +0100

    feat: add closing dropdown

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import {GoChevronDown, GoChevronUp} from "react-icons/go";
-import {Panel} from "./Panel";
+import Panel from "./Panel";
 
 function Dropdown ({options, value, onChange}) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const divRef = useRef();
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (!divRef.current) {
+        return;
+      }
+      
+      if (!divRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    }
+  }, []);
 
   const handleClick = () => {
     setIsOpen((currentIsOpen) => !currentIsOpen);
@@ -21,7 +41,7 @@ function Dropdown ({options, value, onChange}) {
     );});
 
   return (
-    <div className="w-48 relative">
+    <div ref={divRef} className="w-48 relative">
       <Panel className="flex justify-between items-center cursor-pointer"onClick={handleClick}>
         {value?.label || "Select..."}
         {isOpen? <GoChevronUp className="text-lg"/>: <GoChevronDown className="text-lg"/>}
