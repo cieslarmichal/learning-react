f5876ba35ec0c6acbe3713ee74182062f18e3101
Michal Cieslar <michalovskyyy@gmail.com>
2024-11-17 22:50:41 +0100

    feat: add route component

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,6 +1,21 @@
+import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropdownPage from "./pages/DropdownPage";
+
 function App () {
   return (
     <div>
+      <Link to="/accordion">Accordion</Link>
+      <Link to="/dropdown">Dropdown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage/>
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage/>
+        </Route>
+      </div>
     </div>
   );
 }
