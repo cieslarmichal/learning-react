2c8832720a889b72049dad4cf204400a14780530
Michal Cieslar <michalovskyyy@gmail.com>
2024-11-17 22:07:30 +0100

    feat: add dropdown page

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,31 +1,6 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
-
 function App () {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (selectedOption) => {
-    setSelection(selectedOption);
-  }
-
-  const options = [
-    {
-      label: "Red",
-      value: "red"
-    },
-    {
-      label: "Green",
-      value: "green"
-    },
-    {
-      label: "Blue",
-      value: "blue"
-    },
-  ];
-
   return (
     <div>
-      <Dropdown options={options} value={selection} onChange={handleSelect}/>
     </div>
   );
 }
