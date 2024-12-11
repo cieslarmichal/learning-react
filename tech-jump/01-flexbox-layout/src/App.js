1cfda2552a4c7215c13b2b15705636cc249072e9
Michał Cieślar <michal.cieslar@schibsted.com>
2024-12-11 16:05:50 +0100

    feat: add first tech jump exercise

---
+++ tech-jump/01-flexbox-layout/src/App.js
@@ -0,0 +1,45 @@
+
+function App() {
+  return (
+    <div className="app">
+      <div className="header item">
+        <h1>HEADER</h1>
+      </div>
+      <div className="main">
+        <div className="menu item">
+          <h2>MENU</h2>
+          <p>ITEM 1</p>
+          <p>ITEM 2</p>
+          <p>ITEM 3</p>
+          <p>ITEM 4</p>
+          <p>ITEM 5</p>
+        </div>
+        <div className="content item">
+          <h2>CONTENT</h2>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at condimentum purus. Donec scelerisque tempus dui non vulputate. Aliquam et erat sed risus tincidunt placerat. Suspendisse quis massa ut dui dictum posuere. In finibus nunc ligula, eget rhoncus diam facilisis eget. Nulla consectetur nisl nisi, et aliquet enim pulvinar quis. Phasellus pulvinar suscipit neque eu feugiat. Curabitur posuere ex in aliquam lacinia. Fusce sit amet fermentum nunc. Curabitur nisl justo, bibendum ut nibh commodo, feugiat ornare est. Donec porttitor tellus a sodales iaculis. Sed urna magna, consectetur a iaculis sed, bibendum in dolor.
+
+Donec condimentum tellus eget odio feugiat, in mattis velit auctor. Sed sit amet mi sem. Nullam mattis pretium velit ac volutpat. Nullam varius scelerisque quam vitae vehicula. Aliquam vel est eget risus imperdiet cursus vel quis quam. Ut venenatis pellentesque arcu, a lobortis nisl. Vivamus erat nisi, dignissim nec dictum ut, cursus nec enim. In lorem ex, venenatis et fermentum vel, ullamcorper eget sem. Fusce efficitur, quam nec semper mattis, sem ex posuere metus, nec efficitur lacus tellus a magna. Nunc viverra ex turpis, eget semper diam suscipit at. Integer mollis lacinia erat in eleifend. Phasellus vel tempus urna.
+
+Nam aliquet sodales ipsum, in congue massa porta at. Sed vulputate non lectus eget molestie. Sed eros leo, tempus a vestibulum nec, gravida efficitur tortor. Pellentesque auctor lectus sit amet elit varius mattis. Quisque justo ipsum, sollicitudin quis sollicitudin a, tempus quis lacus. Curabitur ultrices risus a risus posuere auctor. Maecenas cursus fringilla sem, vel tempor dui tincidunt at. Nullam nisi massa, aliquam id lectus a, suscipit consectetur purus.
+
+Ut dictum lorem a nisi tincidunt, et tempus velit fringilla. Aenean pellentesque malesuada placerat. Duis auctor felis nec nibh tristique consequat. Quisque dapibus rhoncus euismod. Pellentesque scelerisque lorem vitae massa euismod, vel malesuada enim tincidunt. Nullam condimentum mattis enim, nec lobortis augue tincidunt sit amet. Sed laoreet molestie erat et posuere. Vivamus felis lectus, maximus dapibus ex id, fringilla porta diam. Nam sollicitudin maximus sem porttitor laoreet. Proin nec posuere turpis. Nam vel metus ut nulla semper ornare. Phasellus ornare ipsum nisl, quis interdum nulla iaculis eu. Praesent non ornare nisi. Vivamus quam nibh, convallis sit amet eros non, dignissim convallis nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum in velit nec turpis consectetur ultricies feugiat blandit nibh.
+
+Donec aliquet in orci a commodo. Ut ac elit viverra, commodo diam at, cursus justo. Integer elementum ultricies aliquam. Fusce sed tellus sapien. Curabitur sollicitudin, augue vitae consequat dictum, ex felis imperdiet enim, non pharetra risus libero at lacus. Pellentesque leo sapien, fringilla sed enim at, finibus semper enim. Duis lobortis pulvinar urna porttitor rutrum. Maecenas pretium, turpis quis cursus dictum, felis massa dictum sapien, nec tempor ipsum metus at nulla. Pellentesque vitae convallis mauris, ac egestas massa. Nunc vestibulum posuere nulla id malesuada. Nam tincidunt neque ut turpis pretium, sed varius orci eleifend. Sed efficitur porta commodo. Morbi hendrerit arcu augue, nec porta sapien dapibus in. Praesent rhoncus lacus non imperdiet mollis.
+
+Donec pellentesque mauris vel felis ultricies auctor. Sed dignissim tempus dui. Vivamus vitae vehicula libero. Nullam ex quam, porta nec odio at, malesuada molestie arcu. Etiam aliquam dolor eros, quis mollis sapien viverra eu. Suspendisse facilisis ligula nec erat molestie, quis rutrum ipsum porta. Proin eleifend dui luctus varius laoreet. Aliquam erat volutpat. Cras quis enim nulla. Proin vestibulum mi eget purus fermentum hendrerit. Cras feugiat dui et felis tempor, sed ornare ligula malesuada. Praesent ac odio sem. Phasellus quis massa vulputate, porta nunc eleifend, varius leo. Fusce ligula lorem, gravida sit amet mauris nec, porta ornare magna. Pellentesque ut ipsum eu nunc rhoncus commodo. Aliquam erat volutpat.
+          </p>
+        </div>
+        <div className="ads item">
+          <p>AD</p>
+          <p>AD</p>
+        </div>
+      </div>
+      <div className="footer item">
+        <h1>FOOTER</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
