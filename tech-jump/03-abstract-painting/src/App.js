120712cc5b281bc414d17a683f62faf0820c6891
Michał Cieślar <michal.cieslar@schibsted.com>
2024-12-13 13:42:36 +0100

    feat: implement abstract painting

---
+++ tech-jump/03-abstract-painting/src/App.js
@@ -0,0 +1,51 @@
+function App() {
+  return (
+    <div className="painting">
+      <div className="painting-column painting-column-1">
+        <div className="painting-row painting-row-1">
+          <div className="painting-cell painting-cell-1"></div>
+          <div className="painting-cell painting-cell-2"></div>
+        </div>
+        <div className="painting-row painting-row-2">
+          <div className="painting-column painting-column-3">
+            <div className="painting-cell painting-cell-10"></div>
+            <div className="painting-cell painting-cell-11"></div>
+            <div className="painting-cell painting-cell-12"></div>
+            <div className="painting-cell painting-cell-22"></div>
+          </div>
+
+          <div className="painting-column painting-column-4">
+            <div className="painting-cell painting-cell-13"></div>
+            <div className="painting-cell painting-cell-14"></div>
+            <div className="painting-cell painting-cell-15"></div>
+            <div className="painting-cell painting-cell-23"></div>
+          </div>
+
+          <div className="painting-column painting-column-5">
+            <div className="painting-cell painting-cell-16"></div>
+            <div className="painting-cell painting-cell-17"></div>
+            <div className="painting-cell painting-cell-18"></div>
+            <div className="painting-cell painting-cell-19"></div>
+            <div className="painting-row painting-row-4">
+              <div className="painting-cell painting-cell-20"></div>
+              <div className="painting-cell painting-cell-21"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="painting-column painting-column-2">
+        <div className="painting-cell painting-cell-3"></div>
+        <div className="painting-cell painting-cell-4"></div>
+        <div className="painting-row painting-row-3">
+          <div className="painting-cell painting-cell-5"></div>
+          <div className="painting-cell painting-cell-6"></div>
+        </div>
+        <div className="painting-cell painting-cell-7"></div>
+        <div className="painting-cell painting-cell-8"></div>
+        <div className="painting-cell painting-cell-9"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
