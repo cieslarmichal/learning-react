893fd4c6954dd34e164c6372524e721a602bdbf2
Michal Cieslar <michalovskyyy@gmail.com>
2024-12-15 19:32:08 +0100

    feat: add users list component

---
+++ media/src/components/UsersList.js
@@ -0,0 +1,14 @@
+function UsersList() {
+  return (
+    <div>
+      <h2>Users List</h2>
+      <ul>
+        <li>John</li>
+        <li>Jane</li>
+        <li>Joe</li>
+      </ul>
+    </div>
+  );
+}
+
+export default UsersList;
