b697d1643c727cbcaf8cc98394d0ae70229a8cca
Michal Cieslar <michalovskyyy@gmail.com>
2024-12-18 23:13:30 +0100

    feat: add skeleton component

--- 07-media/src/store/thunks/fetchUsers.js
+++ 07-media/src/store/thunks/fetchUsers.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3005/users');
 
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
   return response.data;
 });
 
